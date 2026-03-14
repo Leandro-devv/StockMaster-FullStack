1e0e6542bb4a474b950ab6516fe52a690af279a6
leandro_caf2@hotmail.com <leandro_caf2@hotmail.com>
2026-03-14 14:17:36 -0300

    Segurança: Protegendo senhas com .env e finalizando a consulta de estoque

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,10 +1,14 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
 
 const conexao = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'inventario'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 export default conexao;
